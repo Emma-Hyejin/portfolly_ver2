56b7c65fd245cba0dd73b43a6aac571ce786a8c6
Emma-Hyejin <cjd191058@naver.com>
2023-10-29 20:17:07 +0900

    Try: useCallback 사용 - 성능 차이 없음

--- client/src/pages/community-detail/CommunityDetail.tsx
+++ client/src/pages/community-detail/CommunityDetail.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import { call } from '@/utils/apiService';
 
@@ -11,6 +11,7 @@ import DeleteModal from '@/components/modal/DeleteModal';
 import { CommuProps } from '@/types';
 
 import { CmDContainer, CommentContainer, TitleContainer, MainContainer, PageWrapper } from './CommunityDetail.styled';
+import axios from 'axios';
 
 export default function CommunityDetail() {
   const [memberData, setMemberData] = useState<CommuProps | null>(null);
@@ -20,26 +21,41 @@ export default function CommunityDetail() {
   const navigate = useNavigate();
   const { id: boardId } = useParams();
   // console.log(boardId);
-
+  const TOKEN = localStorage.getItem('accessToken');
   const handleRender = () => {
     setRender(!render);
   };
 
-  useEffect(() => {
-    const findBoardsById = (id: string) => call(`/boards/${id}`, 'GET', null);
-    const getMember = async () => {
-      //axiosMember(xxx) 이름에서부터 유추 (한글 직독직 -> 영어)
-      return findBoardsById(boardId as string)
-        .then((res) => {
-          setMemberData(res);
-          console.log(res);
-        })
-        .catch((err) => console.log('커뮤니티 상세 페이지 예시' + err));
-    };
+  const findBoardsById = (id: string) => call(`/boards/${id}`, 'GET', null);
 
+  const getMember = useCallback(async() => {
+    await findBoardsById(boardId as string)
+    .then((res) => {
+      setMemberData(res);
+      console.log(res);
+    })
+    .catch((err) => console.log(err));
+  }, [setMemberData]);
+
+  useEffect(() => {
     getMember();
-    console.log(memberData);
-  }, [boardId, render]);
+  }, [getMember, boardId])
+
+  // useEffect(() => {
+  //   const findBoardsById = (id: string) => call(`/boards/${id}`, 'GET', null);
+  //   const getMember = async () => {
+  //     //axiosMember(xxx) 이름에서부터 유추 (한글 직독직 -> 영어)
+  //     return findBoardsById(boardId as string)
+  //       .then((res) => {
+  //         setMemberData(res);
+  //         console.log(res);
+  //       })
+  //       .catch((err) => console.log('커뮤니티 상세 페이지 예시' + err));
+  //   };
+
+  //   getMember();
+  //   console.log(memberData);
+  // }, [boardId, render]);
 
   if (!memberData)
     return (
